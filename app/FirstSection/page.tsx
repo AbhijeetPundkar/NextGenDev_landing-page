333216c2fd97de0a13c595ad88f6f378cd9b9fea
AbhijeetPundkar <abhijeetpundkar02@gmail.com>
2024-08-22 15:46:27 +0530

    init

---
+++ app/FirstSection/page.tsx
@@ -0,0 +1,44 @@
+const FirstSection = () => {
+    return ( 
+    <section className="md:py-20 py-10 bg-gradient-to-r from gray-00 to-gray-200 spacey-10">
+        <div className="container mx-auto text-center">
+            <div className="text-6xl flex justify-center font-bold md:px-20 pb-10
+            text-gradient
+            bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500
+            bg-clip-text
+            text-transparent
+
+
+            
+            ">
+                Build a brand and start selling in seconds
+
+            </div>
+
+            <p className="text-lg md:text-xl md-10
+            text-gray-300
+            bg-clip-text
+            text-transparent
+            font-bold
+            
+            "
+            >
+                   No matter what you sell,
+            FastDev has everything you need to run your online store.
+            </p>
+            <div className="flex gap-4 justify-center pt-10">
+                <button className="bg-blue-500 text-white px-10 py-4 rounded-md text-lg font-bold">Get Started</button>
+                <button className="bg-gray-600 text-white px-10 py-4 rounded-md text-lg font-bold">Learn More</button>
+
+            </div>
+
+            <div className="pt-10">
+
+            </div>
+
+        </div>
+    
+    </section> );
+}
+ 
+export default FirstSection;
---
+++ app/Navbar/page.tsx
@@ -0,0 +1,19 @@
+import ActionButtons from "./action-buttons";
+import Logo from "./logo";
+import { NavigationMenuBar } from "./navigation-bar";
+import { GrDeploy } from "react-icons/gr";
+
+const Navbar = () => {
+    return ( <div className="
+    flex
+    justify-between
+    items-center px-10 border-b h-40
+    
+    ">
+       <Logo />
+       <NavigationMenuBar />
+       <ActionButtons />
+    </div> );
+}
+ 
+export default Navbar;
